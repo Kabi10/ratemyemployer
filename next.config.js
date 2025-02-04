97f34c36557dcfaa6387e4b6cc10105460cc0eca
Kabi <48044253+Kabi10@users.noreply.github.com>
2025-02-04 10:02:16 -0800

    chore: add eslint.ignoreDuringBuilds config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const bundleAnalyzer = require('@next/bundle-analyzer');
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,6 +14,14 @@ const nextConfig = {
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    // Add alias for '@' to point to 'src' directory
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   },
 };
 
